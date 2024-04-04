898828f49c1a91e9e844baa91bc54655f4ac1eac
scarecrow-404 <kittipoom.put@gmail.com>
2024-04-04 20:17:35 +0700

    feat:add dto for customer

--- domain/customer.go
+++ domain/customer.go
@@ -1,6 +1,9 @@
 package domain
 
-import "github.com/scarecrow-404/banking/errs"
+import (
+	"github.com/scarecrow-404/banking/dto"
+	"github.com/scarecrow-404/banking/errs"
+)
 
 type Customer struct {
 	Id          string `db:"customer_id"`
@@ -11,8 +14,30 @@ type Customer struct {
 	Status      string
 }
 
+func (c Customer) statusAstext() string {
+	statusAstext := "active"
+	if c.Status == "0" {
+		statusAstext = "inactive"
+	}
+	return statusAstext
+}
+
+func (c Customer) ToDto() dto.CustomerResponse {
+	
+	return dto.CustomerResponse{
+		Id : c.Id,
+		Name: c.Name,
+		City: c.City,
+		Zipcode: c.Zipcode,
+		DateofBirth: c.DateofBirth,
+		Status: c.statusAstext(),
+	}
+
+}
+
 type CustomerRepository interface {
 	FindAll(status string) ([]Customer, *errs.AppError)
 	ById(string) (*Customer, *errs.AppError)
+
 	// FindStatus(string) ([]Customer, *errs.AppError)
 }
